6223db7a695429ea59d569b12feca790fb05554e
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2025-09-12 18:24:16 +0500

    [chatg-backend-java-se]: Initialized a maven project to implement the chatg backend using java se

---
+++ chatg-backend-java-se/src/main/java/io/github/nturbo1/chatg/ChatgApplication.java
@@ -0,0 +1,13 @@
+package io.github.nturbo1.chatg;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChatgApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
